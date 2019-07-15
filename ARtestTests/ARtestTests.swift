6f1623e305f5cb7f59865c7892d81693f28b773b
DavideDellaMonica <52746946+DavideDellaMonica@users.noreply.github.com>
2019-07-15 10:54:21 +0200

    Add files via upload

---
+++ ARtestTests/ARtestTests.swift
@@ -0,0 +1,34 @@
+//
+//  ARtestTests.swift
+//  ARtestTests
+//
+//  Created by Davide Della Monica on 12/07/2019.
+//  Copyright © 2019 Davide Della Monica. All rights reserved.
+//
+
+import XCTest
+@testable import ARtest
+
+class ARtestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
